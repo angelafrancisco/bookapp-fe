3e46791f18ee36f1ceb48c7c270aeafde024382b
Angela Francisco <angelat.francisco@gmail.com>
2022-04-29 00:03:35 -0700

    adding SASS and responsive design

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -8,27 +8,34 @@ const Header = (props) => {
     }
     return (
         <nav>
-            <div className="nav-container">
+            <div className="nav-container left">
                 <Link to="/" className="nav-logo"><img src="favicon.ico" alt="Logo" />.readME</Link>
+                {props.isLoggedIn ?
+                    <ul className="nav-links-list">
+                        <li><Link to="/books" className="nav-link">My Books</Link></li>
+                        <li><Link to="/books" className="nav-link">Search</Link></li>
+                    </ul>
+                :
+                    <ul className="nav-links-list">
+                        <li><Link to="/" className="nav-link">About</Link></li>
+                        <li><Link to="/" className="nav-link">Features</Link></li>
+                        <li><Link to="/login" className="nav-link">Search</Link></li>
+                    </ul>
+                }
             </div>
-            <div className="nav-container">
-                {/* mobile hamburger nav */}
+            <div className="nav-container right">
                 <div onClick={toggleShowing} className="nav-hamburger-container">
                     <div className="nav-hamburger bar1"></div>
                     <div className="nav-hamburger bar2"></div>
                     <div className="nav-hamburger bar3"></div>
                 </div>
                 {props.isLoggedIn ?
-                    <ul className="nav-links-list isloggedin">
-                        <li><Link to="/books" className="nav-link">Home</Link></li>
-                        <li><Link to="/books" className="nav-link">My Books</Link></li>
-                        <li><Link to="/books" className="nav-link">Search</Link></li>
+                    <ul className="nav-links-list">
+                        <li><Link to="/books" className="outline-btn account">Account</Link></li>
                         <li><Link to="/" className="logout"><button onClick={() => props.setIsLoggedIn(false)} className="solid-btn">Logout</button></Link></li>
                     </ul>
-                    :
-                    <ul className="nav-links-list guest">
-                        <li><Link to="/" className="nav-link">About</Link></li>
-                        <li><Link to="/login" className="nav-link">Search</Link></li>
+                :
+                <ul className="nav-links-list">
                         <li><Link to="/login" className="outline-btn">Login</Link></li>
                         <li><Link to="/register" className="solid-btn">Register</Link></li>
                     </ul>
