1d29d5371fe20ff92ad537f1dfff40179bf79c7a
Angela Francisco <angelat.francisco@gmail.com>
2022-04-30 14:22:56 -0700

    updating feature icons and rd

--- src/components/home/features.jsx
+++ src/components/home/features.jsx
@@ -1,28 +1,34 @@
 const Features = () => {
     return (
         <div className="content-container features">
-            <h2 className="heading">Search for books, create custom book lists, get book recommendations and more!</h2>
+            <h2 className="heading features">Search for books, add custom book lists, get book recommendations and more!</h2>
             <div className="features-grid">
                 <div className="single-feature-box">
                     <div className="feature-img">
-                        <img src="#" alt="icon" />
+                        <img src="./img/feature-1.png" alt="icon" />
                     </div>
-                    <h4 className="feature-title">Feature 1</h4>
-                    <p className="feature-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsam magni iusto soluta impedit vero accusantium modi inventore dolores ducimus!</p>
+                    <div className="feature-detail">
+                        <h3 className="feature-title">Search books</h3>
+                        <p className="feature-text">Lorem ipsum dolor sit amet, consectetur elit. Ipsam magni modi iusto soluta vero inventore dolores!</p>
+                    </div>
                 </div>
                 <div className="single-feature-box">
                     <div className="feature-img">
-                        <img src="#" alt="icon" />
+                        <img src="./img/feature-2.png" alt="icon" />
                     </div>
-                    <h4 className="feature-title">Feature 2</h4>
-                    <p className="feature-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsam magni iusto soluta impedit vero accusantium modi inventore dolores ducimus!</p>
+                    <div className="feature-detail">
+                        <h3 className="feature-title">Save favorites</h3>
+                        <p className="feature-text">Lorem ipsum dolor sit amet, consectetur elit. Ipsam magni modi iusto soluta vero inventore dolores!</p>
+                    </div>
                 </div>
                 <div className="single-feature-box">
                     <div className="feature-img">
-                        <img src="#" alt="icon" />
+                        <img src="./img/feature-3.png" alt="icon" />
                     </div>
-                    <h4 className="feature-title">Feature 3</h4>
-                    <p className="feature-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsam magni iusto soluta impedit vero accusantium modi inventore dolores ducimus!</p>
+                    <div className="feature-detail">
+                        <h3 className="feature-title">Add custom books</h3>
+                        <p className="feature-text">Lorem ipsum dolor sit amet, consectetur elit. Ipsam magni modi iusto soluta vero inventore dolores!</p>
+                    </div>
                 </div>
             </div>
         </div>
