2bca2b9cdf1718196acc3895f3afc9dcfb67964c
Angela Francisco <angelat.francisco@gmail.com>
2022-05-01 18:40:39 -0700

    updating modals and rd

--- src/components/books/bookShelf.jsx
+++ src/components/books/bookShelf.jsx
@@ -21,33 +21,35 @@ const BookShelf = (props) =>{
             <h5 className="book-img-title">{props.book.title}</h5>
             {/* showing = false until edit button clicked */}
             <Modal isOpen={showingBook}>
-                <div className="book-view-modal">
+                <div className="book-form view">
                     <button onClick={() => setShowingBook(false)} className="outline-btn">X</button>
                     <div className="book-view-container">
                         <img className="book-img view" src={props.book.image || "./img/book-default.jpeg"} alt={props.book.title} />
                         <div className="book-view-box">
-                            <h3 className="book-text title">{props.book.title}</h3>
-                            <p className="book-text author">Author: {props.book.author}</p>
-                            <p className="book-text desc">Description: {props.book.description}</p>
-                            <p className="book-text status">{props.book.read}</p>
-                            <p className="book-text list">List Name: {props.book.listName}</p>
+                            <h2 className="book-text title">{props.book.title}</h2>
+                            <p className="book-text author"><span className="bold-text">Author: </span>{props.book.author}</p>
+                            <p className="book-text desc"><span className="bold-text">Description: </span><span className="italic-text">{props.book.description}</span></p>
+                            <p className="book-text status"><span className="bold-text">Current Status: </span>{props.book.read}</p>
+                            <p className="book-text list"><span className="bold-text">List Name: </span>{props.book.listName}</p>
                         </div>
                     </div>
-                    {/* UPDATE/EDIT BUTTON */}
-                    <button onClick={() => props.setShowingForm(true)} className="outline-btn">Edit</button>
-                    {/* UPDATE/EDIT FORM */}
-                    <BookForm
-                        key={props.book._id}
-                        book={props.book}
-                        updateBook={props.updateBook}
-                        isNewBook={false}
-                        showingForm={props.showingForm}
-                        setShowingForm={props.setShowingForm}
-                        closeForm={() => props.setShowingForm(false)}
-                        buttonText={"Update"} 
-                    />
-                    {/* DELETE BUTTON */}
-                    <button onClick={deleteAndCloseModal} className="solid-btn">Delete</button>
+                    <div className="edit-del-buttons">
+                        {/* UPDATE/EDIT BUTTON */}
+                        <button onClick={() => props.setShowingForm(true)} className="outline-btn">Edit</button>
+                        {/* UPDATE/EDIT FORM */}
+                        <BookForm
+                            key={props.book._id}
+                            book={props.book}
+                            updateBook={props.updateBook}
+                            isNewBook={false}
+                            showingForm={props.showingForm}
+                            setShowingForm={props.setShowingForm}
+                            closeForm={() => props.setShowingForm(false)}
+                            buttonText={"Update"} 
+                        />
+                        {/* DELETE BUTTON */}
+                        <button onClick={deleteAndCloseModal} className="solid-btn">Delete</button>
+                    </div>
                 </div>
             </Modal>
         </div>
